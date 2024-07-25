396bc329257fa4516f02d061d082ee168b912784
sinnosuke2701 <ehgml1111@naver.com>
2024-07-25 15:15:43 +0900

    first

---
+++ src/com/mycompany/ch01/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.mycompany.ch01;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to hello world");
+	}
+
+}
