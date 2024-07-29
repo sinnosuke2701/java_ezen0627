ab8f73f21a2a814569829a92f1ffd9d495eecd52
sinnosuke2701 <ehgml1111@naver.com>
2024-07-29 10:11:19 +0900

    first

---
+++ src/com/mycompany/ch02/ConstantsEx.java
@@ -0,0 +1,14 @@
+package com.mycompany.ch02;
+
+public class ConstantsEx {
+	
+	public static final double PAI = 3.14;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		PAI = 11.1 ;
+		
+	}
+
+}
