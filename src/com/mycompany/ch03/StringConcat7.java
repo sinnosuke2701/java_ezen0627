39d8967b7a192f7d258b48e6760a688d96056745
sinnosuke2701 <ehgml1111@naver.com>
2024-07-30 16:55:57 +0900

    ch03

--- src/com/mycompany/ch03/StringConcat7.java
+++ src/com/mycompany/ch03/StringConcat7.java
@@ -12,17 +12,17 @@ public class StringConcat7 {
 		String stringval5 = "10";
 		
 		String stringval3 = stringva1 + stringval2;
-		System.out.println("stringval3 : " + stringval3);
-		System.out.println("신짱구는 " + stringval3);
+			System.out.println("stringval3 : " + stringval3);
+			System.out.println("신짱구는 " + stringval3);
 		
 		int a = 1;
 		
-		String stringval4 = stringva1 + a;
-		System.out.println("stringval4 : " + stringval4);
+			String stringval4 = stringva1 + a;
+			System.out.println("stringval4 : " + stringval4);
 		
 		String stringval6 = stringval5 + a;
 //		int stringval6 = Integer.parseInt(stringval5) + a
-		System.out.println("stringval6 : " + stringval6);
+			System.out.println("stringval6 : " + stringval6);
 		
 	}
 
