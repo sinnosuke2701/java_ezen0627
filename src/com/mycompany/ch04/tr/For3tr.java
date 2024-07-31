69b1b8491a88ccb8afd61b2fe26d5f1410af45b3
sinnosuke2701 <ehgml1111@naver.com>
2024-07-31 12:50:35 +0900

    ch04tr

--- src/com/mycompany/ch04/tr/For3tr.java
+++ src/com/mycompany/ch04/tr/For3tr.java
@@ -5,6 +5,49 @@ public class For3tr {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		// 1. 10*5 별 찍기
+		// **********
+
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= 10; j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+		System.out.println("----------------");
+
+		// 2. 1,10 번째 별 찍기
+		// **********
+		// * 		*
+		// * 		*
+		// * 		*
+		// **********
+
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= 10; j++) {
+				if (i == 1 || i == 5) {
+					System.out.print("*");
+				} else {
+					if (j == 1 || j == 10) {
+						System.out.print("*");
+					} else {
+						System.out.print(" ");
+					}
+				}
+			}
+			System.out.println("");
+		}
+		System.out.println("----------------");
+		
+		//3.
+		//1
+		//12
+		//...
+		//123456789
+		//1 3 5 7 9 0
+		
+		
+		
 	}
 
 }
