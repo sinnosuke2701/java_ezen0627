02a6fa378d769571f2fb8e4e53fed974ff0eb5f0
sinnosuke2701 <ehgml1111@naver.com>
2024-08-01 15:42:16 +0900

    ch05Function

---
+++ src/com/mycompany/ch05Function/Function.java
@@ -0,0 +1,47 @@
+package com.mycompany.ch05Function;
+
+public class Function {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	//public 리턴자료형 메소드형 (입력자료형1 입력변수1, 입력자료형2 입력변수2, ...)	
+	//return 리턴값; //리턴자료형이 void 인 경우네는 return 문이 필요없다.
+	
+		
+	//***************************	
+		 
+	//void = return 값이 없을 때 사용	
+//		int a = 5;
+//		int b = 10;
+		 
+		int result = sum(5,10);
+		int result2 = sum(8,46);
+		System.out.println(result);
+		System.out.println(result2);
+		
+		say();
+		
+		sayhello();
+	}
+	
+	public static int sum (int a , int b) {
+		int sum = a + b ;
+//		System.out.println("sum: " + sum);
+		return sum;
+		
+	}
+	
+	public static void say() {
+		System.out.println("HI");
+	}
+	public static String sayhello() {
+		return "Hello";
+	}
+	
+	public static void sum2(int a ,int b) {
+		int sum = a + b ;
+		System.out.println(a + "+" + b + " = " + sum);
+	}
+	
+}
---
+++ src/com/mycompany/ch05Function/Function3.java
@@ -0,0 +1,89 @@
+package com.mycompany.ch05Function;
+
+public class Function3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// 세 수를 주면 최대값을 반환하는 함수
+
+//		int result = max(1, 7, 5);
+//		System.out.println("최대값은: " + result);
+//		
+//		int result2 = free(23, 23 , 16);
+//		System.out.println("최대값은: " + result2);
+		
+		free(1, 3, 3);
+	}
+
+	public static int max(int a, int b, int c) {
+		int max = 0;
+		if (a != b && a != c && b != c) {
+			if (a > b && a > c) {
+				max = a;
+			} else if (b > a && b > c) {
+				max = b;
+			} else {
+				max = c;
+			}
+		}
+		return max;
+	}
+
+	
+	
+	
+	
+	public static void free(int d, int e, int f) {
+		int free = 0;
+		if (d != e && d != f && e != f) {
+			if (d > e && d > f) {
+				free = d;
+				System.out.println(free);
+			} else if (e > d && e > f) {
+				free = e;
+				System.out.println(free);
+			} else{
+				free = f;
+				System.out.println(free);
+			}
+		} else {
+			System.out.println("같은 값이 2개 이상 존재합니다. 다시 입력하세요.");
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//	public static int free(int d, int e, int f) {
+//		int free = 0;
+//		if (d != e && d != f && e != f) {
+//			if (d > e && d > f) {
+//				free = d;
+//			} else if (e > d && e > f) {
+//				free = e;
+//			} else if (f > d && f > e) {
+//				free = f;
+//			} else {
+//				
+//			}
+//		}
+//		return free;
+//	}
+
+}
