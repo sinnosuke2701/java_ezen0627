4c1010137ab115732116d1515a6a2bd2d48daf9f
sinnosuke2701 <ehgml1111@naver.com>
2024-08-01 12:16:16 +0900

    ch04tr

--- src/com/mycompany/ch04/tr/Switch1tr.java
+++ src/com/mycompany/ch04/tr/Switch1tr.java
@@ -5,6 +5,30 @@ public class Switch1tr {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int num = 14;
+
+		switch (num) {
+		case 10:{
+			System.out.println(num + "번 입니다.");
+			break;
+		}
+		case 11:{
+			System.out.println(num + "번 입니다.");
+			break;
+		}
+		case 12:{
+			System.out.println(num + "번 입니다.");
+			break;
+		}
+		case 13:{
+			System.out.println(num + "번 입니다.");
+			break;
+		}
+		default :
+			System.out.println("10 ~ 13 까지만 가능합니다.");
+			break;
+		}
+
 	}
 
 }
