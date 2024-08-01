e3dbc18227d077ce6507f9890fee2ba52d7ed88a
sinnosuke2701 <ehgml1111@naver.com>
2024-08-01 12:11:53 +0900

    ch04

--- src/com/mycompany/ch04/If5.java
+++ src/com/mycompany/ch04/If5.java
@@ -12,43 +12,43 @@ public class If5 {
 		int a = 2;
 		int b = 5;
 		int c = 1;
-//		
-//		int big = a;
-//		int small = a;
-//		
-//		if (b > big) {
-//			big = b;
-//		} if (b < small) {
-//			small = b;
-//		} if (c > big) {
-//			big = c;
-//		} if (c < small) {
-//			small = c;
-//		} else {
-//
-//		} 
-//			System.out.println("최대값은 : " + big);
-//			System.out.println("최소값은 : " + small);
+		
+		int big = a;
+		int small = a;
+		
+		if (b > big) {
+			big = b;
+		} if (b < small) {
+			small = b;
+		} if (c > big) {
+			big = c;
+		} if (c < small) {
+			small = c;
+			System.out.println("최소값은 : " + small);
+			System.out.println("최대값은 : " + big);
+		} else {
+			System.out.println("동일한 정수가 존재합니다. 수정해주세요.");
+		} 
 			
-			if(a != b && b != c && c != a) {
-				if(a > b && a > c) {
-					System.out.println("최대값은 : " + a);
-				} else if (b > a && b > c) {
-					System.out.println("최대값은 : " + b);
-				} else {
-					System.out.println("최대값은 : " + c);
-				}
-				
-				if(a < b && a < c) {
-					System.out.println("최소값은 : " + a);
-				} else if ( b < a && b < c ) {
-					System.out.println("최소값은 : " + b);
-				} else {
-					System.out.println("최소값은 : " + c);
-				}
-			} else {
-				System.out.println("적어도 2개의 동일한 정수가 존재 합니다. 수정해 주세요");
-			}
+//			if(a != b && b != c && c != a) {
+//				if(a > b && a > c) {
+//					System.out.println("최대값은 : " + a);
+//				} else if (b > a && b > c) {
+//					System.out.println("최대값은 : " + b);
+//				} else {
+//					System.out.println("최대값은 : " + c);
+//				}
+//				
+//				if(a < b && a < c) {
+//					System.out.println("최소값은 : " + a);
+//				} else if ( b < a && b < c ) {
+//					System.out.println("최소값은 : " + b);
+//				} else {
+//					System.out.println("최소값은 : " + c);
+//				}
+//			} else {
+//				System.out.println("적어도 2개의 동일한 정수가 존재 합니다. 수정해 주세요");
+//			}
 	}
 
 }
