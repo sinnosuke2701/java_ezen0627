e92fe5f455bcd0e5b0422096d7ab2086919c1380
sinnosuke2701 <ehgml1111@naver.com>
2024-08-07 17:11:20 +0900

    ch07third

---
+++ src/com/mycompany/ch07/third/ArithemticExtr.java
@@ -0,0 +1,17 @@
+package com.mycompany.ch07.third;
+
+public class ArithemticExtr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Arithemtictr arithemtictr = new Arithemtictr();
+		
+		System.out.println("arithemtictr.plus: " + arithemtictr.plus(9, 5));
+		System.out.println("arithemtictr.minus: " + arithemtictr.minus(9, 5));
+		System.out.println("arithemtictr.multiply: " + arithemtictr.multiply(9, 5));
+		System.out.println("arithemtictr.divide: " + arithemtictr.divide(9, 5));
+		
+	}
+
+}
---
+++ src/com/mycompany/ch07/third/Arithemtictr.java
@@ -0,0 +1,29 @@
+package com.mycompany.ch07.third;
+
+public class Arithemtictr {
+
+	//사칙연산 함수 생성(+,-,*,/,%)
+	
+	//method
+	
+	int plus(int a , int b) {
+		int plus = a + b;
+		return plus;
+	}
+	
+	int minus(int a , int b) {
+		int minus = a - b;
+		return minus;
+	}
+	
+	int multiply(int a , int b) {
+		int multiply = a * b;
+		return multiply;
+	}
+	
+	int divide(int a , int b) {
+		int divide = a / b;
+		return divide;
+	}
+	
+}
