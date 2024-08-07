6d3410b4b34c9f52c51ecabe396dff810417a7ee
sinnosuke2701 <ehgml1111@naver.com>
2024-08-07 12:10:20 +0900

    ch07eight

---
+++ src/com/mycompany/ch07/eight/Constants.java
@@ -0,0 +1,7 @@
+package com.mycompany.ch07.eight;
+
+public class Constants {
+	
+	public static final int TOP_BULDING = 828;
+	
+}
---
+++ src/com/mycompany/ch07/eight/ConstantsEx.java
@@ -0,0 +1,12 @@
+package com.mycompany.ch07.eight;
+
+public class ConstantsEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Constants.TOP_BULDING: " + Constants.TOP_BULDING);
+		
+	}
+
+}
