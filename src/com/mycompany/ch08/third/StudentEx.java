d558ea1c50e658d0bb22a16aaf34c2c335896589
sinnosuke2701 <ehgml1111@naver.com>
2024-08-08 14:04:52 +0900

    ch08third

--- src/com/mycompany/ch08/third/StudentEx.java
+++ src/com/mycompany/ch08/third/StudentEx.java
@@ -5,7 +5,10 @@ public class StudentEx {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		People people = new People();
 		
+		
+		System.out.println();
 		
 	}
 
