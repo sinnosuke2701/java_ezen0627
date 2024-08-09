7af9500e6a7768430f6cc599a5ba7416afb30ec4
sinnosuke2701 <ehgml1111@naver.com>
2024-08-09 11:51:22 +0900

    ch07eighttr

---
+++ src/com/mycompany/ch07/eight/tr/Constants.java
@@ -0,0 +1,7 @@
+package com.mycompany.ch07.eight.tr;
+
+public class Constants {
+	
+	public static final int SHINNOSUKE_AGE = 5;
+	
+}
---
+++ src/com/mycompany/ch07/eight/tr/ConstantsEx.java
@@ -0,0 +1,12 @@
+package com.mycompany.ch07.eight.tr;
+
+public class ConstantsEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Constants.AGE: " + Constants.SHINNOSUKE_AGE);
+		
+	}
+
+}
