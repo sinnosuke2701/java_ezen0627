d6cc4620cc0e9e36dfc1435eb0bf7feeac4bde84
sinnosuke2701 <ehgml1111@naver.com>
2024-08-09 12:06:25 +0900

    ch07ninthtr

---
+++ src/com/mycompany/ch07/ninth/tr/Member.java
@@ -0,0 +1,6 @@
+package com.mycompany.ch07.ninth.tr;
+
+public class Member {
+	
+	
+}
---
+++ src/com/mycompany/ch07/ninth/tr/MemberEx.java
@@ -0,0 +1,10 @@
+package com.mycompany.ch07.ninth.tr;
+
+public class MemberEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
